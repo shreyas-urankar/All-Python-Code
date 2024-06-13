c094e116218acf5cb66430bb41ea2e4187fa6d83
Shreyas <urankarshreyas@gmail.com>
2024-06-14 00:18:19 +0530

    First commit

---
+++ 140-Code-Challenge/Program20.py
@@ -0,0 +1,15 @@
+# Write a Python Program to Find Armstrong Number in an Interval.
+lower = int(input("Enter the lower limit of the interval: "))
+upper = int(input("Enter the upper limit of the interval: "))
+
+for num in range(lower, upper + 1):  # Iterate through the numbers in the range
+    order = len(str(num))  # Find the number of digits in 'num'
+    temp_num = num
+    sum = 0
+    while temp_num > 0:
+        digit = temp_num % 10
+        sum += digit ** order
+        temp_num //= 10
+    # Check if 'num' is an Armstrong number
+    if num == sum:
+        print(num)
